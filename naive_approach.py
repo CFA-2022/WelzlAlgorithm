049cb2e92d1832071e5ae67573c6f0818d224368
trungbui2307 <tb23073010>
2022-01-25 22:28:29 +0100

    Circle

--- naive_approach.py
+++ naive_approach.py
@@ -1,3 +1,4 @@
+import matplotlib.patches
 from matplotlib import pyplot as plt
 from random import randint
 from math import atan2
@@ -5,22 +6,18 @@ from math import atan2
 def create_points(points, min=200, max=500):
     return [[randint(min, max), randint(min, max)] for _ in range(points)]
 
-def scatter_plot(coords, convex_hull=None):
+def scatter_plot(coords, center, radius):
     xs,ys=zip(*coords)
-    plt.scatter(xs, ys)
-    if convex_hull != None:
-        for i in range(1, len(convex_hull) + 1):
-            if i == len(convex_hull): i = 0
-            c0 = convex_hull[i - 1]
-            c1 = convex_hull[i]
-            plt.plot((c0[0], c1[0]), (c0[1], c1[1]), 'r')
+    fig, ax = plt.subplots()
+    ax.scatter(xs, ys)
+    circle = plt.Circle(center, radius, color="r", fill=False)
+    ax.set_aspect('equal', adjustable='datalim')
+    ax.add_patch(circle)
     plt.show()
     print(f"{xs} et {ys}")
 
-
-
 if __name__ == '__main__':
     points = create_points(256)
-    print(points)
-    hull = [256, 410], [425, 312], [230, 260], [303, 428], [466, 207], [414, 328], [382, 347]
-    scatter_plot(points, hull)
+    center = (300,300)
+    radius = 150
+    scatter_plot(points, center, radius)
